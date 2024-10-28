57c89118eeaf51eea3fcc7bdf7b56020e488f418
safeerkhattak <safeerahmad5454@gmail.com>
2024-10-28 23:17:46 +0500

    linked list insert methode

--- LinkedList.js
+++ LinkedList.js
@@ -26,16 +26,46 @@ class LinkedList {
       this.tail = newNode
       return this
   }
+//  2 - 4 - 6
+  insert(index ,value){
+        if(index ===0){
+            return this.prepend(value)
+        }
+        const requiredNode = this.traverseToIndex(index-1)
+        const newNode = {
+            value:value,
+            next:requiredNode.next
+        }
+        
+        requiredNode.next = newNode
+        return this
+
+  }
+
+  traverseToIndex(index){
+
+    let counter = 0
+    let currentNode = this.head
+    while(counter!==index){
+        currentNode = currentNode.next
+        counter++
+    }
+    return currentNode
+
+  }
 
 }
 
-const linkedList = new LinkedList(2)
+const linkedList = new LinkedList(1)
 
+console.log("append",linkedList.append(2))
 console.log("append",linkedList.append(4))
 
 console.log("append",linkedList.append(6))
 
-console.log("append",linkedList.prepend(1))
 console.log("append complete",JSON.stringify(linkedList.append(8)))
+
+
+console.log("append complete",JSON.stringify(linkedList.insert(2,3)))
 
 
